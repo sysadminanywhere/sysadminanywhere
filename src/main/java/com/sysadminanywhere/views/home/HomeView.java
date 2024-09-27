26f334f7403f797031f930ffcdcd2788af865760
sysadminanywhere <info@sysadminanywhere.com>
2024-09-27 16:14:12 +0300

    fixes

--- src/main/java/com/sysadminanywhere/views/home/HomeView.java
+++ src/main/java/com/sysadminanywhere/views/home/HomeView.java
@@ -1,11 +1,11 @@
 package com.sysadminanywhere.views.home;
 
+import com.sysadminanywhere.service.LdapService;
 import com.sysadminanywhere.views.MainLayout;
-import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -16,25 +16,52 @@ import jakarta.annotation.security.PermitAll;
 @PageTitle("Home")
 @Route(value = "", layout = MainLayout.class)
 @RouteAlias(value = "home", layout = MainLayout.class)
+@RouteAlias(value = "dashboard", layout = MainLayout.class)
 @PermitAll
 public class HomeView extends VerticalLayout {
 
-    public HomeView() {
-        setSpacing(false);
+    private final LdapService ldapService;
 
-        Image img = new Image("images/sa-logo.png", "logo");
-        img.setWidth("160px");
-        add(img);
+    public HomeView(LdapService ldapService) {
+        this.ldapService = ldapService;
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setWidthFull();
 
-        H2 header = new H2("Sysadmin Anywhere");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        //add(new Paragraph("Application makes it so much easier to administer Active Directory-based networks"));
+        H3 lblDomain = new H3();
+        lblDomain.setText(ldapService.getDomainName().toUpperCase());
+        lblDomain.setWidth("100%");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        H5 lblDistinguishedName = new H5();
+        lblDistinguishedName.setText(ldapService.getDefaultNamingContext().toUpperCase());
+        lblDistinguishedName.setWidth("100%");
+
+        FormLayout formLayout = new FormLayout();
+
+        TextField txtComputers = new TextField("Computers");
+        txtComputers.setReadOnly(true);
+        txtComputers.setValue(String.valueOf(ldapService.search("(objectClass=computer)").size()));
+
+        TextField txtUsers = new TextField("Users");
+        txtUsers.setReadOnly(true);
+        txtUsers.setValue(String.valueOf(ldapService.search("(&(objectClass=user)(objectCategory=person))").size()));
+
+        TextField txtGroups = new TextField("Groups");
+        txtGroups.setReadOnly(true);
+        txtGroups.setValue(String.valueOf(ldapService.search("(objectClass=group)").size()));
+
+        TextField txtPrinters = new TextField("Printers");
+        txtPrinters.setReadOnly(true);
+        txtPrinters.setValue(String.valueOf(ldapService.search("(objectClass=printQueue)").size()));
+
+        TextField txtContacts = new TextField("Contacts");
+        txtContacts.setReadOnly(true);
+        txtContacts.setValue(String.valueOf(ldapService.search("(&(objectClass=contact)(objectCategory=person))").size()));
+
+        formLayout.add(txtComputers, txtUsers, txtGroups, txtPrinters, txtContacts);
+
+        verticalLayout.add(lblDomain, lblDistinguishedName, formLayout);
+
+        add(verticalLayout);
     }
 
 }
