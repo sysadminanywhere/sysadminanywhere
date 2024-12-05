bb4ad16a01c8be37f79ee70547082e5db4c0f940
sysadminanywhere <info@sysadminanywhere.com>
2024-12-05 09:26:14 +0300

    inventory

--- src/main/java/com/sysadminanywhere/client/InventoryClient.java
+++ src/main/java/com/sysadminanywhere/client/InventoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "inventory", url = "http://localhost:8081")
+@FeignClient(value = "inventory", url = "${sysadminanywhere.inventory.address}")
 public interface InventoryClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/inventory/software/count")
