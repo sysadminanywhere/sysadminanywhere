d485520d499350d8831b7ec1ea3114b00e469520
sysadminanywhere <info@sysadminanywhere.com>
2025-02-16 12:35:55 +0300

    add group dialog

---
+++ src/main/java/com/sysadminanywhere/views/management/groups/AddGroupDialog.java
@@ -0,0 +1,97 @@
+package com.sysadminanywhere.views.management.groups;
+
+import com.sysadminanywhere.control.ContainerField;
+import com.sysadminanywhere.model.GroupEntry;
+import com.sysadminanywhere.model.GroupScope;
+import com.sysadminanywhere.service.GroupsService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.radiobutton.RadioGroupVariant;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class AddGroupDialog extends Dialog {
+
+    private final GroupsService groupsService;
+
+    public AddGroupDialog(GroupsService groupsService, Runnable onSearch) {
+        this.groupsService = groupsService;
+
+        setHeaderTitle("New group");
+        setMaxWidth("800px");
+
+        FormLayout formLayout = new FormLayout();
+
+        ContainerField containerField = new ContainerField(groupsService.getLdapService());
+        containerField.setValue(groupsService.getDefaultContainer());
+        formLayout.setColspan(containerField, 2);
+
+        TextField txtName = new TextField("Name");
+        txtName.setRequired(true);
+        formLayout.setColspan(txtName, 2);
+
+        TextField txtDescription = new TextField("Description");
+        formLayout.setColspan(txtDescription, 2);
+
+        RadioButtonGroup<String> radioGroupScope = new RadioButtonGroup<>();
+        radioGroupScope.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+        radioGroupScope.setLabel("Group scope");
+        radioGroupScope.setItems("Global", "Local", "Universal");
+        radioGroupScope.setValue("Global");
+
+        RadioButtonGroup<String> radioGroupType = new RadioButtonGroup<>();
+        radioGroupType.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+        radioGroupType.setLabel("Group type");
+        radioGroupType.setItems("Security", "Distribution");
+        radioGroupType.setValue("Security");
+
+        formLayout.add(containerField, txtName, txtDescription, radioGroupScope, radioGroupType);
+        add(formLayout);
+
+        Button saveButton = new Button("Save", e -> {
+            GroupEntry group = new GroupEntry();
+            group.setCn(txtName.getValue());
+            group.setDescription(txtDescription.getValue());
+
+            GroupScope scope = GroupScope.Global;
+
+            switch (radioGroupScope.getValue()) {
+                case "Global":
+                    scope = GroupScope.Global;
+                    break;
+                case "Local":
+                    scope = GroupScope.Local;
+                    break;
+                case "Universal":
+                    scope = GroupScope.Universal;
+                    break;
+            }
+
+            try {
+                GroupEntry newGroup = groupsService.add(containerField.getValue(), group, scope, radioGroupType.getValue().equals("Security") ? true : false);
+
+                onSearch.run();
+
+                Notification notification = Notification.show("Computer added");
+                notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            } catch (Exception ex) {
+                Notification notification = Notification.show(ex.getMessage());
+                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+            }
+
+            close();
+        });
+
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        Button cancelButton = new Button("Cancel", e -> close());
+        getFooter().add(cancelButton);
+        getFooter().add(saveButton);
+
+    }
+
+}
--- src/main/java/com/sysadminanywhere/views/management/groups/GroupsView.java
+++ src/main/java/com/sysadminanywhere/views/management/groups/GroupsView.java
@@ -1,10 +1,6 @@
 package com.sysadminanywhere.views.management.groups;
 
-import com.sysadminanywhere.control.ContainerField;
-import com.sysadminanywhere.domain.FilterSpecification;
-import com.sysadminanywhere.model.ComputerEntry;
 import com.sysadminanywhere.model.GroupEntry;
-import com.sysadminanywhere.model.GroupScope;
 import com.sysadminanywhere.service.GroupsService;
 import com.sysadminanywhere.views.MainLayout;
 import com.vaadin.flow.component.Component;
@@ -13,30 +9,20 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
-import com.vaadin.flow.component.radiobutton.RadioGroupVariant;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import jakarta.annotation.security.PermitAll;
-import jakarta.persistence.criteria.*;
 import org.springframework.data.domain.PageRequest;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @PageTitle("Groups")
 @Route(value = "management/groups", layout = MainLayout.class)
@@ -164,80 +150,7 @@ public class GroupsView extends Div {
         }
 
         private Dialog addDialog(Runnable onSearch) {
-            Dialog dialog = new Dialog();
-
-            dialog.setHeaderTitle("New group");
-            dialog.setMaxWidth("800px");
-
-            FormLayout formLayout = new FormLayout();
-
-            ContainerField containerField = new ContainerField(groupsService.getLdapService());
-            containerField.setValue(groupsService.getDefaultContainer());
-            formLayout.setColspan(containerField, 2);
-
-            TextField txtName = new TextField("Name");
-            txtName.setRequired(true);
-            formLayout.setColspan(txtName, 2);
-
-            TextField txtDescription = new TextField("Description");
-            formLayout.setColspan(txtDescription, 2);
-
-            RadioButtonGroup<String> radioGroupScope = new RadioButtonGroup<>();
-            radioGroupScope.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
-            radioGroupScope.setLabel("Group scope");
-            radioGroupScope.setItems("Global", "Local", "Universal");
-            radioGroupScope.setValue("Global");
-
-            RadioButtonGroup<String> radioGroupType = new RadioButtonGroup<>();
-            radioGroupType.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
-            radioGroupType.setLabel("Group type");
-            radioGroupType.setItems("Security", "Distribution");
-            radioGroupType.setValue("Security");
-
-            formLayout.add(containerField, txtName, txtDescription, radioGroupScope, radioGroupType);
-            dialog.add(formLayout);
-
-            Button saveButton = new Button("Save", e -> {
-                GroupEntry group = new GroupEntry();
-                group.setCn(txtName.getValue());
-                group.setDescription(txtDescription.getValue());
-
-                GroupScope scope = GroupScope.Global;
-
-                switch (radioGroupScope.getValue()) {
-                    case "Global":
-                        scope = GroupScope.Global;
-                        break;
-                    case "Local":
-                        scope = GroupScope.Local;
-                        break;
-                    case "Universal":
-                        scope = GroupScope.Universal;
-                        break;
-                }
-
-                try {
-                    GroupEntry newGroup = groupsService.add(containerField.getValue(), group, scope, radioGroupType.getValue().equals("Security") ? true : false);
-
-                    onSearch.run();
-
-                    Notification notification = Notification.show("Computer added");
-                    notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-                } catch (Exception ex) {
-                    Notification notification = Notification.show(ex.getMessage());
-                    notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
-                }
-
-                dialog.close();
-            });
-
-            saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-            Button cancelButton = new Button("Cancel", e -> dialog.close());
-            dialog.getFooter().add(cancelButton);
-            dialog.getFooter().add(saveButton);
-
-            return dialog;
+            return new AddGroupDialog(groupsService, onSearch);
         }
 
     }
