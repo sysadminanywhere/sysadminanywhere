bb4035909cb47370b036c8b37b5c783385b9bb23
sysadminanywhere <info@sysadminanywhere.com>
2025-03-21 17:36:49 +0300

    email service

---
+++ src/main/java/com/sysadminanywhere/service/EmailService.java
@@ -0,0 +1,25 @@
+package com.sysadminanywhere.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendMessage(String from, String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+}
