538719a158a8707b3ad111acc572c223c12104b7
sysadminanywhere <info@sysadminanywhere.com>
2025-10-20 15:37:32 +0300

    Refactoring

--- main/src/main/java/com/sysadminanywhere/config/KeycloakFeignClientInterceptor.java
+++ main/src/main/java/com/sysadminanywhere/config/KeycloakFeignClientInterceptor.java
@@ -2,13 +2,20 @@ package com.sysadminanywhere.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 
+@RequiredArgsConstructor
 @Slf4j
 public class KeycloakFeignClientInterceptor implements RequestInterceptor {
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_TOKEN_TYPE = "Bearer";
@@ -16,15 +23,20 @@ public class KeycloakFeignClientInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof DefaultOidcUser) {
-            DefaultOidcUser oidcUser = (DefaultOidcUser) authentication.getPrincipal();
-            template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, oidcUser.getIdToken().getTokenValue()));
+
+        if (authentication instanceof OAuth2AuthenticationToken oauthToken) {
+            String registrationId = oauthToken.getAuthorizedClientRegistrationId();
+            OAuth2AuthorizedClient client = authorizedClientService
+                    .loadAuthorizedClient(registrationId, oauthToken.getName());
+
+            if (client != null && client.getAccessToken() != null) {
+                String accessToken = client.getAccessToken().getTokenValue();
+                template.header(AUTHORIZATION_HEADER, BEARER_TOKEN_TYPE + " " + accessToken);
+            }
         }
 
         log.info("Feign Request: {} {}", template.method(), template.url());
-        log.info("Headers: {}", template.headers());
-        if (template.body() != null) {
-            log.info("Body: {}", new String(template.body()));
-        }
+        log.debug("Headers: {}", template.headers());
     }
+
 }
