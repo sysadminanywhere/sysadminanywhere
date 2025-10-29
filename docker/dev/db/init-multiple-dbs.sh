eed852daf7c88179cc4d94197f37f6835bd5a4bf
DIASOFT\ivmarkin <ivmarkin@diasoft.ru>
2025-10-29 15:32:31 +0300

    db fix

--- docker/dev/db/init-multiple-dbs.sh
+++ docker/dev/db/init-multiple-dbs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Создаём дополнительные базы
 for db in keycloak inventory monitoring; do
   echo "Creating database '$db'"
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
