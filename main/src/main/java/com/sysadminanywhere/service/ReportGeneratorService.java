9a2b0e3f8f156e088b38850a50a626157c24a915
sysadminanywhere <info@sysadminanywhere.com>
2026-03-08 12:38:33 +0300

    Refactoring

--- main/src/main/java/com/sysadminanywhere/service/ReportGeneratorService.java
+++ main/src/main/java/com/sysadminanywhere/service/ReportGeneratorService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
