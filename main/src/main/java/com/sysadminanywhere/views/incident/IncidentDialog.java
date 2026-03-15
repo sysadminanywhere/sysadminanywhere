2b9412063804063a2978085f1ab785daafd990c7
sysadminanywhere <info@sysadminanywhere.com>
2026-03-15 10:33:14 +0300

    Refactoring

--- main/src/main/java/com/sysadminanywhere/views/incident/IncidentDialog.java
+++ main/src/main/java/com/sysadminanywhere/views/incident/IncidentDialog.java
@@ -3,9 +3,12 @@ package com.sysadminanywhere.views.incident;
 import com.sysadminanywhere.common.incident.model.IncidentItem;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.textfield.TextField;
+
+import java.util.List;
 
 public class IncidentDialog extends Dialog {
 
@@ -21,25 +24,28 @@ public class IncidentDialog extends Dialog {
 
         TextField txtName = new TextField("Name");
         txtName.setValue(incident.getName());
+        txtName.setReadOnly(true);
         formLayout.setColspan(txtName, 2);
 
         TextField txtMachineName = new TextField("Machine name");
         txtMachineName.setValue(incident.getMachineName());
+        txtName.setReadOnly(true);
         formLayout.setColspan(txtMachineName, 2);
 
-        TextField txtSeverity = new TextField("Severity");
-        txtSeverity.setValue(incident.getSeverity().name());
-        formLayout.setColspan(txtSeverity, 2);
+        ComboBox<String> comboSeverity = new ComboBox<>("Severity");
+        comboSeverity.setItems(List.of("Low", "Medium", "High", "Critical"));
+        comboSeverity.setValue(incident.getSeverity().name());
 
-        TextField txtStatus = new TextField("Status");
-        txtStatus.setValue(incident.getStatus().name());
-        formLayout.setColspan(txtStatus, 2);
+        ComboBox<String> comboStatus = new ComboBox<>("Status");
+        comboStatus.setItems(List.of("Open", "In Progress", "Resolved", "False Positive", "Closed"));
+        comboStatus.setValue(incident.getStatus().name().replace("_", " "));
 
         TextField txtRecommendation = new TextField("Recommendation");
         txtRecommendation.setValue(incident.getRecommendation());
+        txtRecommendation.setReadOnly(true);
         formLayout.setColspan(txtRecommendation, 2);
 
-        formLayout.add(txtName, txtMachineName, txtSeverity, txtStatus, txtRecommendation);
+        formLayout.add(txtName, txtMachineName, comboSeverity, comboStatus, txtRecommendation);
 
         add(formLayout);
 
