2b0472731288f568379de71f9a51646d9301211a
sysadminanywhere <info@sysadminanywhere.com>
2026-03-21 11:35:44 +0300

    Refactoring

--- docker/prod/db/init-multiple-dbs.sh
+++ docker/prod/db/init-multiple-dbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for db in incident inventory; do
+for db in incident inventory n8n; do
   echo "Creating database '$db'"
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
 CREATE DATABASE $db;
